87da8ff465ebe7abb0ee311fef9738690b45e7b9
Esa <esa-pitkanen@hotmail.com>
2026-02-04 14:37:02 +0200

    feat: add GameBoard component
    - component stores initialData to useState
    - The fetchPokemon function retrieves an image from the Pokemon API
    - With the useEffect hook, all images are retrieved at the same time
    - Added card components to the game board

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { initialData } from "../data/data";
+import Card from "./Card";
+
+export default function GameBoard() {
+  const [data, setData] = useState(initialData);
+
+  const fetchPokemon = async (pokemon) => {
+    const url = `https://pokeapi.co/api/v2/pokemon/${pokemon}/`;
+    const response = await fetch(url, { mode: "cors" });
+    const json = await response.json();
+    const source = json.sprites.front_default;
+    return source;
+  };
+
+  useEffect(() => {
+    let ignore = false;
+
+    const fetchAllPokemons = async () => {
+      const results = await Promise.all(
+        data.map(async (item) => {
+          const image = await fetchPokemon(item.name);
+
+          return { ...item, imgSrc: image };
+        }),
+      );
+
+      if (!ignore) {
+        setData(results);
+      }
+    };
+
+    fetchAllPokemons();
+
+    return () => {
+      ignore = true;
+    };
+  }, []);
+
+  return (
+    <div className="card-board">
+      {data.map((item) => (
+        <Card data={item} />
+      ))}
+    </div>
+  );
+}
