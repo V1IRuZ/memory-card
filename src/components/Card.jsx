5f3a6fc1e592d5e85df22412e2764963effe587e
Esa <esa-pitkanen@hotmail.com>
2026-02-04 19:43:31 +0200

    feat(card): add click event and onShuffle prop

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
-export default function Card({ data }) {
+export default function Card({ data, onShuffle }) {
   return (
-    <div className="card" key={data.id}>
+    <div className="card" key={data.id} onClick={onShuffle}>
       <h1>{data.name}</h1>
       <img src={data.imgSrc} alt="" />
     </div>
