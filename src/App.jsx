a50a9dafbe2d6c30b2131d19f5cee92cd8a5a94c
Esa <esa-pitkanen@hotmail.com>
2026-02-05 19:19:08 +0200

    feat: add highscore state

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,28 @@ import GameBoard from "./components/GameBoard";
 import "./App.css";
 
 export default function App() {
+  const [highScore, setHighScore] = useState(5);
   const [score, setScore] = useState(0);
 
   return (
     <div className="page">
       <header>
         <h1>Memory Card</h1>
+        <div className="high-score">
+          <span>Highscore: </span>
+          <span>{highScore}</span>
+        </div>
         <div className="score">
           <span>Points:</span>
           <span>{score}</span>
         </div>
       </header>
-      <GameBoard setScore={setScore} />
+      <GameBoard
+        score={score}
+        setScore={setScore}
+        highScore={highScore}
+        setHighScore={setHighScore}
+      />
     </div>
   );
 }
