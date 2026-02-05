1f93eb992357bf64a4ba3aa29c21623ebad366aa
Esa <esa-pitkanen@hotmail.com>
2026-02-05 18:32:49 +0200

    feat: add score state

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
+import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import "./App.css";
 
-
 export default function App() {
+  const [score, setScore] = useState(0);
 
   return (
     <div className="page">
       <header>
         <h1>Memory Card</h1>
+        <div className="score">
+          <span>Points:</span>
+          <span>{score}</span>
+        </div>
       </header>
-      <GameBoard />
+      <GameBoard setScore={setScore} />
     </div>
   );
 }
