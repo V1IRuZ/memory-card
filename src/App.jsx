57f1df54944acdee973699f4adf4ec04610039a3
Esa <esa-pitkanen@hotmail.com>
2026-02-07 13:58:01 +0200

    feat: add states to control the Scoreboard component

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,30 @@
 import { useState } from "react";
+import { initialScores } from "./data/data";
 import GameBoard from "./components/GameBoard";
+import Scroreboard from "./components/Scroreboard";
 import "./App.css";
 
 export default function App() {
-  const [highScore, setHighScore] = useState(5);
+  const [highScore, setHighScore] = useState(initialScores);
   const [score, setScore] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
 
   return (
     <div className="page">
       <header>
         <h1>Memory Card</h1>
-        <div className="high-score">
-          <span>Highscore: </span>
-          <span>{highScore}</span>
-        </div>
         <div className="score">
           <span>Points:</span>
           <span>{score}</span>
         </div>
+        <button className="highscore" onClick={() => setIsOpen(true)}>
+          High Scores
+        </button>
+        {isOpen && <Scroreboard highScores={highScore} onClose={handleClose} />}
       </header>
       <GameBoard
         score={score}
