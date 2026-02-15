7c0c56b228cdbd3c8adca42ffc9c0adec6829ccd
Esa <esa-pitkanen@hotmail.com>
2026-02-15 09:56:29 +0200

    feat: add text prop to Button component

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,7 +1,14 @@
-export default function Button({ className, iconSrc, arialLabel, onClick }) {
+export default function Button({
+  className,
+  text,
+  iconSrc,
+  arialLabel,
+  onClick,
+}) {
   return (
     <button className={className} aria-label={arialLabel} onClick={onClick}>
       {iconSrc && <img src={iconSrc} alt="" />}
+      {text && text}
     </button>
   );
 }
